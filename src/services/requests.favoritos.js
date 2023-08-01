f03c340c770dec2e5f4d08cb6ab52f4e86cbfc23
Thiago Souza <thiago29.06@hotmail.com>
2023-07-31 22:50:49 -0300

    correção da url de importação de favoritos

--- src/services/requests.favoritos.js
+++ src/services/requests.favoritos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const favoritosAPI = axios.create({ baseURL: "http://localhost:8000/livros" })
+const favoritosAPI = axios.create({ baseURL: "http://localhost:8000/favoritos" })
 
 async function getFavoritos() {
   const response = await favoritosAPI.get('/')
